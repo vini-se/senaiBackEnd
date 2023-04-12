f2af72c4f914a519a63eda5d02820d2e3bbcd60a
Vinicius <vinicius.dla2106@gmail.com>
2023-04-12 14:17:53 -0300

    Primeiro escopo para filtrar por marca

--- DevAPIs/searchCarPlate/back/index.js
+++ DevAPIs/searchCarPlate/back/index.js
@@ -18,9 +18,7 @@ fs.readFile("./carsPlate_DB.json", (err, data) => {
 });
 
 // Rota do site que você deseja fazer as requisições
-server.use(cors({
-  origin: 'http://127.0.0.1:5500'
-}));
+server.use(cors());
 
 // Endpoint para MOSTRAR todos os carros
 server.get("/cars", (req, res) => {
@@ -31,7 +29,7 @@ server.get("/cars", (req, res) => {
 server.get("/car/placa/:id", (req, res) => {
   const placa = req.params.id; // Extrai o valor do param id para "placa"
 
-  const car = carros.cars.find((carro) => carro.placa === placa); // Procura pelo carro com a placa fornecida
+  const car = carros.cars.find( (carro) => carro.placa === placa ); // Procura pelo carro com a placa fornecida
 
   const retorno = { data: car } // Estrutura do retorno
 
--- DevAPIs/searchCarPlate/front/index.js
+++ DevAPIs/searchCarPlate/front/index.js
@@ -1,19 +1,27 @@
 let placaCampo = document.getElementById("placa");
 let ano, marca, modelo;
 
-function getCarro(placa) {
+function validarPlaca() {
   const placa = placaCampo.value.toUpperCase();
+  const placaRegex = /^[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}$/; // Define a expressão regular
+  const placaValida = placaRegex.test(placa);
 
+  if (placaValida) {
+    getCarro(placa);
+    return;
+  }
+  alert("Tipo de placa inválida!\nPorfavor digite 'LLLNLNN'");
+}
+
+function getCarro(placa) {
   const url = `http://localhost:3000/car/placa/${placa}`;
 
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-
       const carro = data.data;
 
       popularHtml(carro);
-
     });
 }
 
@@ -40,3 +48,16 @@ function setCampos() {
   placa = document.getElementById("placa");
 }
 
+function getMarcas(){
+
+  // endpoint para pegar no backend as marcas !!!
+  var options = { option1: "Opção 1", option2: "Opção 2", option3: "Opção 3", option4: "Opcao do banco" };
+  
+  var datalist = document.getElementById("myList");
+  
+  for (var key in options) {
+    var option = document.createElement("option");
+    option.value = options[key];
+    datalist.appendChild(option);
+  }
+}
