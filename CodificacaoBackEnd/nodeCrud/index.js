333eb2d2d58cb89fd2f1ae1ccb2e85603b0d731e
Vinicius <vi058590@intelbras.com.br>
2023-06-24 00:03:40 -0300

    dunno what is

--- CodificacaoBackEnd/nodeCrud/index.js
+++ CodificacaoBackEnd/nodeCrud/index.js
@@ -13,37 +13,52 @@ server.use((req, res, next) => {
 
 server.listen(3000);
 
-function checkCurso(req, res, next) {
-  if (!req.body.newCurso) {
-    return res.status(400).json({ error: "Nome do curso faltando!" });
+function checkIndexCurso(req, res, next) {
+  const { id } = req.params;
+
+  if (!cursos[id]) {
+    return res.status(400).json({
+      error: `Indice do curso indisponível! Último índice da lista de cursos: ${
+        cursos.length - 1
+      }`,
+    });
+  }
+
+  return next();
+}
+
+function checkBodyCurso(req, res, next) {
+  const { body } = req;
+
+  if (!body.hasOwnProperty("newCurso")) {
+    return res
+      .status(400)
+      .json({ error: "Estrutura errada! Cheque como dever ser a solicitação" });
   }
   return next();
 }
 
-function checkCursos(req, res, next) {
-  if (!req.body.newCursos) {
-    return res.status(400).json({ error: "Estrutura errada! Cheque como dever ser a solicitação" });
+function checkBodyCursos(req, res, next) {
+  const { body } = req;
+
+  if (!body.hasOwnProperty("newCursos")) {
+    return res
+      .status(400)
+      .json({ error: "Estrutura errada! Cheque como dever ser a solicitação" });
   }
   return next();
 }
 
 function checkCursoCursos(req, res, next) {
-  const cursos = req.body.newCursos
+  const cursos = req.body.newCursos;
 
-  if(cursos.find( (curso) => curso.newCurso == undefined )){
-    return res.status(400).json({ error: "Estrutura errada! Cheque como dever ser a solicitação" });
+  if (!cursos.find((curso) => curso.hasOwnProperty("newCurso"))) {
+    // if(cursos.find( (curso) => curso.newCurso == undefined) ){
+    return res
+      .status(400)
+      .json({ error: "Estrutura errada! Cheque como dever ser a solicitação" });
   }
 
-  return next();
-}
-
-function checkIndexCurso(req, res, next){
-  const { id } = req.params;
-
-  if(!cursos[id]){
-    return res.status(400).json({ error: `Indice do curso indisponível! Último índice da lista de cursos: ${cursos.length - 1 }` });
-  }
-  
   return next();
 }
 
@@ -53,21 +68,21 @@ server.get("/cursos", (req, res) => {
 });
 
 // GET -> Envia apenas um curso
-server.get("/curso/:id", checkIndexCurso, (req, res) => {
+server.get("/curso/:id", (req, res) => {
   const { id } = req.params;
 
-  return res.json(cursos[id]);
+  return res.json({ curso: cursos[id] });
 });
 
 // POST -> Cria um curso
-server.post("/newcurso", checkCurso, (req, res) => {
+server.post("/newcurso", checkBodyCurso, (req, res) => {
   const { newCurso } = req.body;
   cursos.push(newCurso);
   return res.json(cursos);
 });
 
 // POST -> Cria vários cursos
-server.post("/newcursos", checkCursoCursos, (req, res) => {
+server.post("/newcursos", checkBodyCursos, checkCursoCursos, (req, res) => {
   const { newCursos } = req.body;
 
   // Modelo do body
@@ -81,11 +96,11 @@ server.post("/newcursos", checkCursoCursos, (req, res) => {
 });
 
 // PUT -> Atualiza um curso da API
-server.put("/curso/:index", checkIndexCurso,(req, res) => {
-  const { index } = req.params;
+server.put("/curso/:id", checkIndexCurso, checkBodyCurso, (req, res) => {
+  const { id } = req.params;
   const { newCurso } = req.body;
 
-  cursos[index] = newCurso;
+  cursos[id] = newCurso;
 
   return res.json({ cursos: cursos });
 });
